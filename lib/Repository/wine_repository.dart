c4080f0b6b397c09598549336667ef239dc26460
lumiere-on <mjw0214@ewhain.net>
2024-01-27 14:52:59 +0900

    모델 수정

--- lib/Repository/wine_repository.dart
+++ lib/Repository/wine_repository.dart
@@ -7,6 +7,38 @@ import 'package:path/path.dart';
 
 // SQLite 데이터베이스에서 특정 카테고리에 맞는 와인 리스트를 검색하여 반환 기랜
 class WineRepository {
+
+  //와인 모델
+  int? id;
+  String? name;
+  String? category;
+  double? price;
+  int? alcohol;
+
+  Future<void> processUserData(List<Map<String, dynamic>> wineData) async {
+    // 사용자의 선택값을 AI모델의 패키지로 보내서 비교.
+    //어떤 속성으로 데이터를 불러올지는 추후에 조정 필요.
+    // 여기에서 AI 모델 초기화 및 작업을 수행
+    for (var wine in wineData) {
+       id=wine['id'];
+       name=wine['name'];
+      category=wine['category'];
+      price=wine['price'];
+      alcohol=wine['alcohol'];
+
+      // 여기에서 AI 모델에 데이터를 전달하고 결과를 얻습니다.
+      // 결과를 사용하여 필요한 작업을 수행합니다.
+      // 더미 메소드입니다.
+      processAIResult(category!);
+    }
+  }
+
+  void processAIResult(String category) {
+    // 여기에서 AI 모델의 결과를 활용하여 작업 수행
+    print('사용자가 선택한 카테고리에 어울리는 안주는  $category');
+  }
+
+
   getWinesByCategory(String category) {}
  // Future<List<Wine>> getWinesByCategory(String category) async {
  // }
