a12a20551b8886d4503fc899e9c0b5831688f6ec
lumiere-on <mjw0214@ewhain.net>
2024-01-28 20:21:25 +0900

    모델 수정

--- lib/Repository/wine_repository.dart
+++ lib/Repository/wine_repository.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:csv/csv.dart';
+import '../Model/database.dart';
+import '../Model/wine.dart';
+
 // SQLite 데이터베이스와의 상호작용을 담당하는 클래스.
 // 예를 들어 특정 카테고리에 속하는 와인을 검색하는 메소드를 제공하는 클래스
 // + 와인 랜덤 추천 메소드 제공
@@ -9,39 +15,82 @@ import 'package:tflite_flutter/tflite_flutter.dart';
 // SQLite 데이터베이스에서 특정 카테고리에 맞는 와인 리스트를 검색하여 반환 기랜
 class WineRepository {
 
-  //와인 모델
-  int? id;
-  String? name;
-  String? mood;
-  String? taste; 
-  String? snack;
-  int? alcohol;
-  double? price;
 
-  //입력값을 AI모델로 바꾸기.
-  Future<void> processUserData(List<Map<String, dynamic>> wineData) async {
-    // 사용자의 선택값을 AI모델의 패키지로 보내서 비교.
-    //어떤 속성으로 데이터를 불러올지는 추후에 조정 필요.
-    // 여기에서 AI 모델 초기화 및 작업을 수행
-    for (var wine in wineData) {
-       id=wine['id'];
-       name=wine['name'];
+  //#전체 와인 데이터(CSV)->DB
+  Future<List<List<dynamic>>> readCsv(String filePath) async {
+    String content = await File(filePath).readAsString();
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(content);
 
-      price=wine['price'];
-      alcohol=wine['alcohol'];
-
-      // 여기에서 AI 모델에 데이터를 전달하고 결과를 얻습니다.
-      // 결과를 사용하여 필요한 작업을 수행합니다.
-      // 더미 메소드입니다.
-      processAIResult(category!);
-    }
+    return csvTable;
   }
 
-  void processAIResult(String category) {
-    // 여기에서 AI 모델의 결과를 활용하여 작업 수행
-    print('사용자가 선택한 카테고리에 어울리는 안주는  $category');
+  Future<void> insertDataFromCsv(List<List<dynamic>> csvData) async {
+    Database database = await openDatabase('mydatabase.db', version: 1, onCreate: (db, version) {
+    // 테이블 생성 등의 초기화 작업 수행
+      db.execute('''
+      CREATE TABLE IF NOT EXISTS mytable (
+        id INTEGER PRIMARY KEY,
+        column1 TEXT,
+        column2 TEXT,
+        column3 INTEGER
+      )
+    ''');
+  });
+
+     Batch batch = database.batch();
+
+    for (List<dynamic> row in csvData) {
+      batch.insert('mytable', {
+        'column1': row[0],
+        'column2': row[1],
+        'column3': row[2],
+     });
+    }
+
+   await batch.commit();
+  }
+
+  //#입력값을 AI모델->DB-> wine 객체로 산출
+  void runModel(List<String> list) {
+    // 모델 초기화
+    TFLiteModelInterpreter.runModelOnBinary(_convertInputToBinary(input));
+    // 결과 처리
+
+    }
+  //1.입력받은 카테고리를 레포지토리로 보내는 것  ==> List<String>
+    2.와인 객체를 화면에 출력하기 위해.  -==> Wine wine
+
+  // 입력값을 이진 데이터로 변환하는 함수
+  Uint8List _convertInputToBinary(List<String> input) {
+  // 예시: 텍스트를 바이트로 변환
+    String id= Uint8List.fromList(utf8.encode(input));
+    getData(database, id);
+  //
+  }
+  Wine getData(String id) async {
+    //1.DB에서 값 가져와서 MAP형태로 변환하기. 
+    //2.Wine객체로 변환하기. 
+    Database database;
+    database=wholeDatabase;
+
+    List<Map<String, dynamic>> result = await database.query('id');
+     //여기서 'wines'가 뭘 의미하느지 수정 필요.
+    x
+    Wine wine;
+    wine=mapToWine(result);
+
+  }
+
+  Wine mapToWine(Map<String, dynamic> map){
+
+    Wine wine;
+    wine['id']=map['id'];
+    wine['name']=map['name'];
+    wine['mood']=map['mood'];
+    wine['taste']=map['snack'];
+    wine['alcohol']=map['alcohol'];
+    wine['price']=map['price'];
+
   }
 
 }
-
-
